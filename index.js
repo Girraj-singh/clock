3d971f96f3c9d24beddbe2f96e7b234d3ad47531
Girraj-singh <sgirraj66@gmail.com>
2022-11-04 23:00:49 +0530

    Analog clock

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(()=>{
+    d = new Date();
+    htime= d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotetion = 30*htime + mtime/2;
+    mrotetion = 6*mtime;
+    srotetion = 6*stime;
+    hour.style.transform = `rotate(${hrotetion}deg)`
+    minute.style.transform = `rotate(${mrotetion}deg)`
+    second.style.transform = `rotate(${srotetion}deg)`
+},500);
